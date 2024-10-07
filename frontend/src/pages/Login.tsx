7617e69628f634d6d0afa022bc2e56f72ceef7d5
javadocq <han18264@naver.com>
2024-10-07 18:51:59 +0900

    Login Page 하단 텍스트 추가

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,13 +1,14 @@
 import 'react-native-gesture-handler';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
   Text,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
-import {CheckBox} from 'react-native-elements';
+import { Checkbox, Provider as PaperProvider } from 'react-native-paper';
 
 interface LoginProps {
   navigation: {
@@ -15,10 +16,11 @@ interface LoginProps {
   };
 }
 
-export default function Login({navigation}: LoginProps): React.JSX.Element {
+export default function Login({ navigation }: LoginProps): React.JSX.Element {
   const [id, setId] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+  const [checked, setChecked] = useState<boolean>(false);
 
   function InputIdHandler(Id: string) {
     setId(Id);
@@ -33,6 +35,18 @@ export default function Login({navigation}: LoginProps): React.JSX.Element {
     console.log('비밀번호:', password);
     navigation.navigate('Main');
   };
+
+  function handleSignup() {
+    navigation.navigate('SignUp');
+  }
+
+  function handleSearchId() {
+    Alert.alert('ID찾기');
+  }
+
+  function handleSearchPW() {
+    Alert.alert('PW찾기');
+  }
 
   return (
     <View style={styles.wrap}>
@@ -49,19 +63,25 @@ export default function Login({navigation}: LoginProps): React.JSX.Element {
         onChangeText={InputPWHandler}
         secureTextEntry={!isPasswordVisible}
       />
-      <CheckBox
-        title="비밀번호 보기"
-        checked={isPasswordVisible}
-        onPress={() => setIsPasswordVisible(!isPasswordVisible)}
-        containerStyle={styles.checkboxContainer} // 체크박스 컨테이너 스타일
-        textStyle={styles.checkboxText} // 텍스트 스타일
-        checkedColor="black"
-        // checkedIcon=""
-        // uncheckedIcon=""
-      />
+      <View style={styles.checkboxContainer}>
+        <Checkbox
+          status={checked ? 'checked' : 'unchecked'}
+          onPress={() => {
+            setChecked(!checked);
+          }}
+        />
+        <Text style={styles.checkboxText}>로그인 정보 저장</Text>
+      </View>
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>로그인</Text>
       </TouchableOpacity>
+      <View style={styles.bottomTextLine}>
+        <Text style={styles.bottomText} onPress={handleSearchId}>아이디 찾기</Text>
+        <Text style={styles.bottomText}>|</Text>
+        <Text style={styles.bottomText} onPress={handleSearchPW}>비밀번호 찾기</Text>
+        <Text style={styles.bottomText}>|</Text>
+        <Text style={styles.bottomTextSign} onPress={handleSignup}>회원가입</Text>
+      </View>
     </View>
   );
 }
@@ -95,15 +115,38 @@ const styles = StyleSheet.create({
     fontWeight: 'normal',
   },
   checkboxContainer: {
-    backgroundColor: 'transparent',
-    borderWidth: 0,
-    marginRight: 200,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent : 'flex-start',
+    width : 320,
+    marginTop: 14,
   },
   checkboxText: {
-    marginLeft: 8,
     color: '#AAA',
     fontSize: 14,
     fontStyle: 'normal',
     fontWeight: 400,
+  },
+  bottomTextLine: {
+    flexDirection: 'row',
+    width: 326,
+    justifyContent: 'center',
+    gap: 10,
+    marginTop: 26,
+  },
+  bottomText: {
+    color: '#7F7F7F',
+    textAlign: 'right',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: 400,
+  },
+  bottomTextSign: {
+    color: '#7F7F7F',
+    textAlign: 'right',
+    fontSize: 14,
+    fontStyle: 'normal',
+    fontWeight: 400,
+    marginTop: 1.4,
   },
 });
