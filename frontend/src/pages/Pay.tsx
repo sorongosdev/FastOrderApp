361433d160295d0d4552b3f744031880b2e1a11e
javadocq <han18264@naver.com>
2024-10-22 13:42:23 +0900

    modal Pop up

--- frontend/src/pages/Pay.tsx
+++ frontend/src/pages/Pay.tsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     SafeAreaView,
     ScrollView,
     Text,
     View,
     TouchableOpacity,
+    Alert,
+    Modal,
+    TextInput,
 } from 'react-native';
 import { NavigationProp } from '../navigation/NavigationProps';
 import BackArrow from '../assets/icon_back_arrow.svg';
 import NextArrow from '../assets/icon_next_arrow.svg';
 import Eclips from '../assets/icon_eclips.svg';
+import Cancel from '../assets/icon_cancel.svg';
 import styles from "../styles/Pay";
 import BottomButton from "../components/BottomButton";
 
 export default function Pay({ navigation }: NavigationProp):React.JSX.Element {
+    const [modalVisible, setModalVisible] = useState(false);
+    const [requestText, setRequestText] = useState('');
+
 
     function handleBack() {
-        navigation.navigate('Shopping')
+        navigation.navigate('Shopping');
+    }
+    function handleReqPopUp() {
+        setModalVisible(true);
+    }
+
+    function handleAlertCoupon() {
+        Alert.alert('현재 가지고 있는 쿠폰이 없습니다.');
+    }
+
+    function handleSubmitRequest() {
+        setRequestText(requestText);
+        setModalVisible(false);
+    }
+    function handleCancelRequest() {
+        setRequestText('');
+        setModalVisible(false);
     }
 
     return (
@@ -35,12 +58,12 @@ export default function Pay({ navigation }: NavigationProp):React.JSX.Element {
 
                 <View>
                     <Text style={styles.labelText}>가게 요청사항</Text>
-                    <View style={styles.inputBox}>
-                        <Text>요청 입력</Text>
+                    <TouchableOpacity style={styles.inputBox} onPress={handleReqPopUp}>
+                        <Text>{requestText || '요청 입력'}</Text>
                         <TouchableOpacity style={styles.nextArrow}>
                             <NextArrow/>
                         </TouchableOpacity>
-                    </View>
+                    </TouchableOpacity>
                     <View style={styles.inputBox}>
                         <TouchableOpacity style={styles.temporal}>
                             <Eclips />
@@ -53,14 +76,14 @@ export default function Pay({ navigation }: NavigationProp):React.JSX.Element {
                         </TouchableOpacity>
                     </View>
                 </View>
-                <View>
+                <TouchableOpacity onPress={handleAlertCoupon}>
                     <Text style={styles.labelText}>할인쿠폰</Text>
                     <View style={styles.inputBox}>
                         <Text>요청 입력</Text>
                     </View>
-                </View>
+                </TouchableOpacity>
 
-                <View>
+                <TouchableOpacity>
                     <Text style={styles.labelText}>현금영수증</Text>
                     <View style={styles.inputBox}>
                         <Text>신청 안함</Text>
@@ -68,32 +91,60 @@ export default function Pay({ navigation }: NavigationProp):React.JSX.Element {
                             <NextArrow/>
                         </TouchableOpacity>
                     </View>
-                </View>
+                </TouchableOpacity>
                 <View>
                     <Text style={styles.labelText}>결제하기</Text>
                     <View style={styles.payInfo}>
-                            <View>
-                                <Text>패패오더 포인트</Text>
-                                <View style={styles.temporal}>
-                                    <Text>총 보유 포인트</Text>
-                                    <Text>50,000 P</Text>
-                                </View>
-                            <View>
-                                <View style={styles.temporal}>
-                                    <Text>결제 포인트</Text>
-                                    <Text>- 28,000 P</Text>
-                                </View>
-                                <View style={styles.temporal}>
-                                    <Text>예상 포인트 잔액</Text>
-                                    <Text>22,000 P</Text>
+                            <View style={styles.whiteBox}>
+                                <Text style={styles.papaPoint}>패패오더 포인트</Text>
+                                <View style={styles.textBox}>
+                                    <Text style={styles.myPoint}>총 보유 포인트</Text>
+                                    <Text style={styles.myPoint}>50,000 P</Text>
                                 </View>
                             </View>
-                        </View>
+                            <View style={styles.grayBox}>
+                                <View style={styles.textBox}>
+                                    <Text style={styles.payPoint}>결제 포인트</Text>
+                                    <Text style={styles.payPoint}>- 28,000 P</Text>
+                                </View>
+                                <View style={styles.textBox}>
+                                    <Text style={styles.payPoint}>예상 포인트 잔액</Text>
+                                    <Text style={styles.payPoint}>22,000 P</Text>
+                                </View>
+                            </View>
                     </View>
                 </View>
             </View>
         </ScrollView>
         <BottomButton name="결제하기" onPress={handleBack} />
+
+        {/* 팝업 모달 */}
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.modalBackground}>
+                    <View style={styles.modalView}>
+                        <View style={styles.modalTextBox}>
+                            <Text style={styles.modalText}>가게 요청사항</Text>
+                            <TouchableOpacity onPress={handleCancelRequest}>
+                                <Cancel />
+                            </TouchableOpacity>
+                        </View>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="예) 양파 빼주세요, 안 맵게 해주세요"
+                            placeholderTextColor={'#484747'}
+                            value={requestText}
+                            onChangeText={setRequestText}
+                        />
+                    </View>
+                </View>
+                <BottomButton name='완료' onPress={handleSubmitRequest} />
+            </Modal>
     </SafeAreaView>
     )
 }   
--- frontend/src/styles/Pay.js
+++ frontend/src/styles/Pay.js
@@ -46,6 +46,7 @@ const styles = StyleSheet.create({
         alignItems : 'center',
         paddingLeft : '3%',
         flexDirection : 'row',
+        position : 'relative'
     },
     mealType : {
         marginLeft : '5%'
@@ -54,7 +55,8 @@ const styles = StyleSheet.create({
         flexDirection : 'row',
     },
     nextArrow : {
-        marginLeft : '75%'
+        position : 'absolute',
+        right : '5%'
     },
     payInfo : {
         width : 325,
@@ -65,8 +67,100 @@ const styles = StyleSheet.create({
         borderRadius : 5,
         marginTop : '2%',
         alignItems : 'center',
-        paddingLeft : '3%',
-    }
+    },
+    whiteBox : {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '60%',
+        backgroundColor: '#FFF',
+        width: '100%', 
+        paddingLeft : '10%',
+        gap : '6%'
+    },
+    papaPoint : {
+        color : '#484747',
+        fontSize : 14,
+        fontWeight : '500',
+        fontStyle : 'normal'
+    },
+    myPoint : {
+        color : '#484747',
+        fontSize : 12,
+        fontWeight : '500',
+        fontStyle : 'normal'
+    },
+    textBox : {
+        width : '90%',
+        flexDirection : 'row',
+        justifyContent : 'space-between'
+    },
+    grayBox : {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#D9D9D9',
+        height: '40%', 
+        width: '100%', 
+        paddingLeft : '10%',
+        gap : '4%'
+    },
+    payPoint : {
+        color : '#484747',
+        fontSize : 12,
+        fontWeight : '400',
+        fontStyle : 'normal'
+    },
+    modalBackground: {
+        flex: 1,
+        justifyContent: 'flex-end', 
+        backgroundColor: 'rgba(217, 217, 217, 0.8)',
+    },
+    modalView: {
+        height : '33%',
+        backgroundColor: 'white',
+        borderRadius: 20,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    modalTextBox : {
+        marginTop : '8%',
+        flexDirection : 'row',
+        width : '84%',
+        justifyContent : 'space-between'
+    },
+    modalText: {
+        fontStyle : 'normal',
+        fontSize: 16,
+        fontWeight: '600',
+        color : '#484747'
+    },
+    input: {
+        marginTop : '5%',
+        height: '20%',
+        width: '84%',
+        borderColor: '#D9D9D9',
+        borderWidth: 1,
+        borderRadius : 5,
+        backgroundColor : 'rgba(217, 217, 217, 0.00)',
+        paddingLeft : '4%'
+    },
+    button: {
+        backgroundColor: '#2196F3',
+        borderRadius: 5,
+        padding: 10,
+        margin: 5,
+        width: '45%',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'white',
+    },
 });
 
 export default styles;
