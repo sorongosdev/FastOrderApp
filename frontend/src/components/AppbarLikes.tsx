4f976c9a2b6485377c12c0b4e6143f48a486d0a3
Sorongos <dosora9@gmail.com>
2024-10-30 13:23:38 -0700

    feat: 찜 앱바에서 장바구니로 이동

--- frontend/src/components/AppbarLikes.tsx
+++ frontend/src/components/AppbarLikes.tsx
@@ -3,13 +3,19 @@ import styles from '../styles/AppbarLikes';
 import {View, Text} from 'react-native';
 /**Icons */
 import CartIcon from '@assets/icon_cart.svg';
+import {NavigationProp} from '../navigation/NavigationProps';
 
-export default function AppbarLikes(): React.JSX.Element {
+export default function AppbarLikes({
+  navigation,
+}: NavigationProp): React.JSX.Element {
+  const navigateToShopping = () => {
+    navigation.navigate('Shopping');
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>ㅇㅇ님의 찜</Text>
       <View style={styles.rightContainer}>
-        <CartIcon />
+        <CartIcon onPress={navigateToShopping} />
       </View>
     </View>
   );
