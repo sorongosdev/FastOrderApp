0beefca16b83063b26c9c091ccb7f974e359180b
Sorongos <dosora9@gmail.com>
2024-10-31 12:31:41 -0700

    feat: 주문목록 리스트에서 리스트아이템 업데이트

--- frontend/src/pages/Order.tsx
+++ frontend/src/pages/Order.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import {NavigationProp} from '../navigation/NavigationProps';
 
@@ -7,17 +7,64 @@ import styles from '../styles/Order';
 /**Components */
 import OrderListItem from '../components/OrderListItem';
 import AppbarDefault from '../components/AppbarDefault';
+import {ScrollView} from 'react-native-gesture-handler';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export default function Order({navigation}: NavigationProp): React.JSX.Element {
+  const [recentMenu, setRecentMenu] = useState([
+    {
+      date: '8.17(수)',
+      progress: '픽업완료',
+      storeName: '찌개찌개',
+      menuName: '김치찌개 외 1개 28,000원',
+    },
+    {
+      date: '8.16(화)',
+      progress: '픽업완료',
+      storeName: '찌개찌개',
+      menuName: '된장찌개 외 1개 27,000원',
+    },
+    {
+      date: '8.15(월)',
+      progress: '픽업완료',
+      storeName: '찌개찌개',
+      menuName: '김치찌개 외 1개 26,000원',
+    },
+    {
+      date: '8.17(수)',
+      progress: '픽업완료',
+      storeName: '찌개찌개',
+      menuName: '김치찌개 외 1개 28,000원',
+    },
+    {
+      date: '8.16(화)',
+      progress: '픽업완료',
+      storeName: '찌개찌개',
+      menuName: '된장찌개 외 1개 27,000원',
+    },
+    {
+      date: '8.15(월)',
+      progress: '픽업완료',
+      storeName: '찌개찌개',
+      menuName: '김치찌개 외 1개 26,000원',
+    },
+  ]);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <AppbarDefault title="주문내역" />
       <View style={styles.divder}></View>
-      <View style={styles.orderListContainer}>
-        <OrderListItem navigation={navigation} />
-        <OrderListItem navigation={navigation} />
-        <OrderListItem navigation={navigation} />
-      </View>
-    </View>
+      <ScrollView style={styles.orderListContainer}>
+        {recentMenu.map((menu, index) => (
+          <OrderListItem
+            navigation={navigation}
+            date={menu.date}
+            progress={menu.progress}
+            storeName={menu.storeName}
+            menuName={menu.menuName}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
