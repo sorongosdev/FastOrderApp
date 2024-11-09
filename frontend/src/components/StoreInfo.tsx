5a998e4b0ec29c86e695dbb9df21f7afa6cbf6c5
Sorongos <dosora9@gmail.com>
2024-11-09 09:29:31 -0800

    feat: 바텀시트에 가게 정보 표출

--- frontend/src/components/StoreInfo.tsx
+++ frontend/src/components/StoreInfo.tsx
@@ -1,10 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
+import axios from 'axios';
+
+/** Consts */
+import {BASE_URL} from '../consts/Url';
+
 import styles from '../styles/StoreInfo';
 import {NavigationProp} from '../navigation/NavigationProps';
 
-/** Styles */
+interface Store {
+  no: number;
+  store_name: string;
+  latitude: string;
+  longitude: string;
+  store_type: string;
+}
 
 interface CombinedProp extends NavigationProp {
   storeId: number | null;
@@ -14,28 +25,51 @@ export default function OrderHistory({
   navigation,
   storeId,
 }: CombinedProp): React.JSX.Element {
-  //TODO: storeId로 특정 매장 조회
+  const [store, setStore] = useState<Store | null>(null);
+
+  const getStoresByType = async (storeId: number) => {
+    try {
+      const response = await axios.get(`${BASE_URL}/stores/mini/id/${storeId}`);
+      setStore(response.data.store_data);
+      console.log(response.data.store_data);
+    } catch (error) {
+      console.error(`Error during getStores of type ${storeId}:`, error);
+    }
+  };
+
+  useEffect(() => {
+    if (storeId) {
+      getStoresByType(storeId);
+    }
+  }, [storeId]); // storeId가 변경될 때마다 실행
+
   return (
     <View style={styles.container}>
-      <View style={styles.storeNameWrapper}>
-        <Text style={styles.storeName}>찌개찌개</Text>
-        <Text style={styles.storeType}>한식</Text>
-      </View>
+      {store ? ( // store가 null이 아닐 때만 렌더링
+        <>
+          <View style={styles.storeNameWrapper}>
+            <Text style={styles.storeName}>{store.store_name}</Text>
+            <Text style={styles.storeType}>{store.store_type}</Text>
+          </View>
 
-      <View style={styles.statusWrapper}>
-        <Text style={styles.businessStatus}>영업중</Text>
-        <Text style={styles.orderStatus}> • 패스트 오더 가능</Text>
-      </View>
+          <View style={styles.statusWrapper}>
+            <Text style={styles.businessStatus}>영업중</Text>
+            <Text style={styles.orderStatus}> • 패스트 오더 가능</Text>
+          </View>
 
-      <ScrollView
-        horizontal
-        contentContainerStyle={styles.imgScrollView}
-        showsHorizontalScrollIndicator={false}>
-        <View style={styles.menuImg}></View>
-        <View style={styles.menuImg}></View>
-        <View style={styles.menuImg}></View>
-        <View style={styles.menuImg}></View>
-      </ScrollView>
+          <ScrollView
+            horizontal
+            contentContainerStyle={styles.imgScrollView}
+            showsHorizontalScrollIndicator={false}>
+            <View style={styles.menuImg}></View>
+            <View style={styles.menuImg}></View>
+            <View style={styles.menuImg}></View>
+            <View style={styles.menuImg}></View>
+          </ScrollView>
+        </>
+      ) : (
+        <Text>Loading...</Text> // 데이터가 로드 중일 때 표시할 내용
+      )}
     </View>
   );
 }
