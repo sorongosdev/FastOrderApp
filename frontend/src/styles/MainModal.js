a91a54f7b25c480bc4616f6e8e710aab4c374d22
sorongosdev <dosora9@gmail.com>
2024-11-10 12:10:46 +0900

    style: 메인 모달 스타일 수정중

---
+++ frontend/src/styles/MainModal.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: 300,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+    width: '100%',
+  },
+});
+
+export default styles;
